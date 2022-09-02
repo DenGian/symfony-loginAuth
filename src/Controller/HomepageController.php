50e3e4205738dffd7ea86f21d7370ae51bb9ab21
DenGian <mondelaers.ian@gmail.com>
2022-09-02 10:39:04 +0200

    add a homepage controller

---
+++ src/Controller/HomepageController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HomepageController extends AbstractController
+{
+    #[Route('/homepage', name: 'app_homepage')]
+    public function index(): Response
+    {
+        return $this->render('homepage/index.html.twig', [
+            'controller_name' => 'HomepageController',
+        ]);
+    }
+    #[Route('/admin', name: 'app_admin')]
+    public function admin(): Response
+    {
+        return $this->render('homepage/index.html.twig', [
+            'controller_name' => 'gg hackerboy',
+        ]);
+    }
+}
